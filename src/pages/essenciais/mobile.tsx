a761d8dd78932200d1d1dfc0d1915626a894d28d
Sebastião Rodrigol <rodrigoexer1@gmail.com>
2024-12-25 23:42:04 -0300

    updated

---
+++ src/pages/essenciais/mobile.tsx
@@ -0,0 +1,18 @@
+export default function Mobile(){
+    return(
+        <div className="
+            text-white
+            bg-zinc-800
+            md:bg-blue-700
+            lg:bg-violet-700
+            h-screen
+            
+            flex
+            items-center
+            justify-center
+        ">       
+
+        <h1 className="text-5xl md:text-zinc-900 md:font-bold flex gap-2">Welcome <span className="hidden md:block">Friends</span> </h1>
+        </div>
+    )
+}
